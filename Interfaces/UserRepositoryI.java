72c8938d1ae09e808bb92819a63684f1428277a5
Esteban Miguel Alvarez <estebabnsoft.dev2001@gmail.com>
2025-05-10 19:39:00 -0300

    .

--- Interfaces/UserRepositoryI.java
+++ Interfaces/UserRepositoryI.java
@@ -2,12 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package Interfaces;
+package sistemacontable.sistemaContable.back.Interfaces;
+
+import sistemacontable.sistemaContable.back.Models.Usuario;
 
 /**
  *
  * @author esteb
  */
 public interface UserRepositoryI {
-    
+    public void insertUser(Usuario usuario);
+    public Usuario getUserById(int id_usuario);
 }
--- Models/Usuario.java
+++ Models/Usuario.java
@@ -2,12 +2,89 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Models;
+package sistemacontable.sistemaContable.back.Models;
 
 /**
  *
  * @author esteb
  */
 public class Usuario {
-    
+    private int dni;
+    private String nombres;
+    private String apellido;
+    private String mail;
+    private String username;
+    private String contrasena;
+    private int idRol;
+
+    public Usuario(){}
+
+    // Constructor con parámetros
+    public Usuario(int dni, String nombres, String apellido, String mail, String username, String contrasena, int idRol) {
+        this.dni = dni;
+        this.nombres = nombres;
+        this.apellido = apellido;
+        this.mail = mail;
+        this.username = username;
+        this.contrasena = contrasena;
+        this.idRol = idRol;
+    }
+
+    // Getters y Setters
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+    public int getIdRol() {
+        return idRol;
+    }
+
+    public void setIdRol(int idRol) {
+        this.idRol = idRol;
+    }
 }
+
