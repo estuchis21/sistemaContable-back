285e03b9ffe345fe0e5a2f6a3d1be8dd643f429a
estuchis01 <estebansoft.dev2001@gmail.com>
2025-08-23 20:58:06 -0300

    .

---
+++ Interfaces/AuthI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package sistemacontable.back.Interfaces;
+
+import sistemacontable.back.Models.Usuario;
+
+/**
+ *
+ * @author Esteban
+ */
+public interface AuthI {
+    public boolean registrar(Usuario users);
+    public Usuario getUserById(int id_usuario);
+}
